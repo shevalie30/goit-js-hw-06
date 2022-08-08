7336598851ac1bd8b74206efa58e9f0798d25ab1
shevalie30 <103785982+shevalie30@users.noreply.github.com>
2022-08-08 22:48:37 +0300

    task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -6,4 +6,14 @@
 выведет в консоль текст заголовка элемента(тега < h2 >) и
 количество элементов в категории(всех вложенных в него < li >).*/
 
+const navEl = document.querySelector(`#categories`);
+console.log(navEl.children);
+const heroTitleEl = navEl.querySelectorAll(`.item`);
+for (let i = 0; i < heroTitleEl.length; i++) {
+    console.log(heroTitleEl[i].querySelector('h2').textContent);
+    console.log(heroTitleEl[i].querySelectorAll('li').length)
+
+
+}
+
 
